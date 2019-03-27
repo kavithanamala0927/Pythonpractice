e3d048bd5022f7434708dd1a840c93e9bd8cba50
kavithanamala0927 <37437691+kavithanamala0927@users.noreply.github.com>
2019-03-27 15:59:38 +0530

    Update strip_function.py

--- RegEx/strip_function.py
+++ RegEx/strip_function.py
@@ -1,1 +1,12 @@
 import re
+
+
+def strip_function(input_string, chars=" "):
+    pattern = "^(" + re.escape(chars) + ")*([\w\d\S\s]*?)("+re.escape(chars)\
+        + ")*$"
+    search = re.search(pattern, input_string)
+    if search:
+        print ("Striped string:" + '"' + search.group(2) + '"')
+    else:
+        print ("Nothhing to strip in the input string:", input_string)
+strip_function("^^^^   This is   ", chars="^")
