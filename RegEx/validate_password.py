7d5826ad8dcdd6a7067a42a9d0f39d57f711eab1
kavithanamala0927 <37437691+kavithanamala0927@users.noreply.github.com>
2019-03-27 15:30:41 +0530

    Update validate_password.py

--- RegEx/validate_password.py
+++ RegEx/validate_password.py
@@ -1,11 +1,12 @@
 import re
 
+
 def validate_password(password):
-    pattern = r"((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[ !\"#$%&'()*+,-.\/:;<=>?@^_`{|}~]).{8,32})"
+    pattern = r"((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[ !\"#$%&'()*+,-.\/:;" +\\
+        "<=>?@^_`{|}~]).{8,32})"
     password_strength = re.search(pattern, password)
     if password_strength:
-	    print ("The password entered is strong and it's a valid")
+        print ("The password entered is strong and it's a valid")
     else:
-	    print ("Not a valid password")
-		
+        print ("Not a valid password")
 validate_password("Arjungowri@719")
