849c4e1bbd4bce6738d5c88580dcd71f43752f46
kavithanamala0927 <37437691+kavithanamala0927@users.noreply.github.com>
2019-03-27 16:15:39 +0530

    Update commas_for_every_3_digits.py

--- RegEx/commas_for_every_3_digits.py
+++ RegEx/commas_for_every_3_digits.py
@@ -1,3 +1,14 @@
+"""
+How would you write a regex that matches a number with commas for
+every three digits? It must match the following:
+• '42'
+• '1,234'
+• '6,368,745'
+but not the following:
+• '12,34,567' (which has only two digits between the commas)
+• '1234' (which lacks commas)
+"""
+
 import re
 
 
